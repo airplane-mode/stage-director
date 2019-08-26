4920c284f90eacb3434b26cd1ad2f3f78033a4d0
Shane Gibbons <airshanemode@gmail.com>
2019-08-26 13:46:26 +0800

    drop expect for built-in assert, write trivial test to make sure things work end to end, better vim swapfile ignores

---
+++ test/tests.js
@@ -0,0 +1,20 @@
+const assert = require("assert");
+const StageDirector = require("../lib/stage-director").default;
+
+
+const director = new StageDirector("test", {
+  update: (state, { value }) => ({
+    ...state,
+    value
+  })
+});
+
+
+describe("Stage Director", () => {
+
+  it("should produce an action when an action creator is called", () => {
+    const action = director.actions.update({ value: "initial" }, { value: "updated" });
+    assert.ok(action);
+  });
+
+});
