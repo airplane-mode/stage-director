8b4bfafa209428b917428958e6d38e21ea5817b7
Shane Gibbons <airshanemode@gmail.com>
2019-08-26 13:37:32 +0800

    remove corejs shims, ignore build in gitignore

--- babel.config.js
+++ babel.config.js
@@ -7,10 +7,6 @@ const presets = [
         firefox: "60",
         chrome: "67",
         safari: "11.1",
-      },
-      useBuiltIns: "usage",
-      corejs: {
-        version: 3
       }
     },
   ],
